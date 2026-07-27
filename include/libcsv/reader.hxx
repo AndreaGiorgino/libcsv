66db8658f5c3b4ee0adfd785509208dd484d15e8
giorgi.page <andreagiorgino1@gmail.com>
2026-07-27 18:46:12 +0200

    reader options and conversion function definitions added

--- include/libcsv/reader.hxx
+++ include/libcsv/reader.hxx
@@ -8,6 +8,13 @@ class reader final {
    public: // definitions
     using container = std::array<T, Nm>;
 
+    struct options final {
+        char separator {','};
+        char quotation {'"'};
+    };
+
+    auto convert(container row) -> T;
+
    public: // ctors
     reader(std::istream& is, options opts = {});
 
@@ -28,4 +35,9 @@ class reader final {
     std::istream& _is;
     std::optional<T> buffer {};
 };
+
+template <typename T, std::size_t Nm>
+auto reader<T, Nm>::convert(reader<T, Nm>::container) -> T {
+    static_assert(true, "Missing convert function definition for type T");
+}
 } // namespace libcsv
