d4a49a4c58b340a83c4d340b565497e08c7a2f00
giorgi.page <andreagiorgino1@gmail.com>
2026-07-28 16:16:09 +0200

    test stream ignore multiple implemented

---
+++ ctest/TestStreamIgnoreMultiple.cxx
@@ -0,0 +1,41 @@
+#include <cassert>
+#include <fstream>
+#include <iostream>
+
+#include "libcsv/reader.hxx"
+
+using namespace libcsv;
+
+struct person {
+    std::string name {};
+    int age {};
+    std::string city {};
+    std::string bio {};
+};
+
+template <>
+inline auto reader<person, 4>::convert(container data) -> person {
+    return {
+        .name = data[0],
+        .age  = std::stoi(data[1]),
+        .city = data[2],
+        .bio  = data[3],
+    };
+}
+
+auto TestStreamIgnoreMultiple(int, char**) -> int {
+    std::ifstream ifs {"test-files/sample-data.csv"};
+    reader<person, 4> r {ifs};
+
+    r.ignore(2);
+
+    const auto p {r.get()};
+    std::cout << p.value().name << std::endl;
+    assert(p.has_value());
+    assert(p.value().name == "Bob \"The Builder\"");
+    assert(p.value().age == 40);
+    assert(p.value().city == "Los Angeles");
+    assert(p.value().bio == "");
+
+    return 0;
+}
