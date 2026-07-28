6faecda86440e09d4bfd4451ac7cd7c33acd89c1
giorgi.page <andreagiorgino1@gmail.com>
2026-07-28 16:30:29 +0200

    test stream seekg implemented

---
+++ ctest/TestStreamSeekg.cxx
@@ -0,0 +1,39 @@
+#include <cassert>
+#include <fstream>
+
+#include "libcsv/reader.hxx"
+
+using namespace libcsv;
+
+struct person {
+    std::string name {};
+    int age {};
+    std::string city {};
+    std::string bio {};
+};
+
+template <>
+inline auto reader<person, 4>::convert(container data) -> person {
+    return {
+        .name = data[0],
+        .age  = std::stoi(data[1]),
+        .city = data[2],
+        .bio  = data[3],
+    };
+}
+
+auto TestStreamSeekg(int, char**) -> int {
+    std::ifstream ifs {"test-files/sample-data.csv"};
+    reader<person, 4> r {ifs};
+
+    r.seekg(85);
+
+    const auto p {r.get()};
+    assert(p.has_value());
+    assert(p.value().name == "Alice");
+    assert(p.value().age == 25);
+    assert(p.value().city == "London");
+    assert(p.value().bio == "Software Engineer");
+
+    return 0;
+}
