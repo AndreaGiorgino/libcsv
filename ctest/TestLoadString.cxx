c557f4bec1dec9c980fe921d93ce78c839cf82be
giorgi.page <andreagiorgino1@gmail.com>
2026-07-28 15:38:33 +0200

    test load string updated

--- ctest/TestLoadString.cxx
+++ ctest/TestLoadString.cxx
@@ -5,17 +5,18 @@
 
 using namespace libcsv;
 
+struct empty {};
+
 template<>
-auto reader<std::nullptr_t, 1>::convert(container) -> std::nullptr_t {
-    return nullptr;
+inline auto reader<empty, 1>::convert(container) -> empty {
+    return {};
 }
 
 auto TestLoadString(int, char**) -> int {
     try {
         std::stringstream ss {""};
 
-        reader<std::nullptr_t, 1> r {ss};
-        r.ignore();
+        reader<empty, 1> r {ss};
     } catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
         return 1;
