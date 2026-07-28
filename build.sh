1c39a17135fd3ac9968d083a818e0b1e8689f2c8
giorgi.page <andreagiorgino1@gmail.com>
2026-07-28 15:35:53 +0200

    default build updated

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-cmake -B build -DCSV_BUILD_TESTING=ON \
-    && cmake --build build --parallel $(($(nproc) - 1)) \
-    && ctest --test-dir build --output-on-failure
+cmake -B build \
+    && cmake --build build --parallel $(($(nproc) - 1))
