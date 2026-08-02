92e4f5f5af45ed4af7aeab835f69d1b99e913b0f
giorgi.page <andreagiorgino1@gmail.com>
2026-08-02 18:17:30 +0200

    test stream malformed implemented

---
+++ ctest/TestStreamMalformed.cxx
@@ -0,0 +1,25 @@
+#include <cassert>
+#include <fstream>
+
+#include "libcsv/reader.hxx"
+
+using namespace libcsv;
+
+struct empty {};
+
+template <>
+inline auto reader<empty, 1>::convert(container) -> empty {
+    return {};
+}
+
+auto TestStreamMalformed(int, char**) -> int {
+    std::ifstream ifs {"test-files/malformed-data.csv"};
+    reader<empty, 4> r {ifs};
+
+    try {
+        while (r.get() != std::nullopt);
+    } catch (const parse_error&) {
+    }
+
+    return 0;
+}
